fb287aa0c1862926b159a0d125718849b55ed020
JUNSEO <djawnstj44@naver.com>
2024-03-27 17:18:14 +0900

    commit

---
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,47 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        final long startTime = System.currentTimeMillis();
+        // 비지니스 로직 실행
+        log.info("비지니스 로직1 실행");
+        // 비지니스 로직 종료
+        final long endTime = System.currentTimeMillis();
+        final long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        final long startTime = System.currentTimeMillis();
+        // 비지니스 로직 실행
+        log.info("비지니스 로직2 실행");
+        // 비지니스 로직 종료
+        final long endTime = System.currentTimeMillis();
+        final long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    @DisplayName("템플릿 메서드 패턴 적용 1")
+    @Test
+    public void templateMethodV1() throws Exception {
+        final AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        final AbstractTemplate template2 = new SubClassLogic1();
+        template2.execute();
+    }
+}
