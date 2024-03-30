06129737aa1f740921219028e7de173e13be8fd9
junseo <djawnstj44@naver.com>
2024-03-30 23:50:38 +0900

    commit

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -44,4 +44,26 @@ public class TemplateMethodTest {
         final AbstractTemplate template2 = new SubClassLogic1();
         template2.execute();
     }
+
+    @DisplayName("템플릿 메서드 패턴 적용 2")
+    @Test
+    public void templateMethodV2() throws Exception {
+        final AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직 1 실행");
+            }
+        };
+        log.info("클래스 이름 1={}", template1.getClass());
+        template1.execute();
+
+        final AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직 2 실행");
+            }
+        };
+        log.info("클래스 이름 2 ={}", template2.getClass());
+        template2.execute();
+    }
 }
